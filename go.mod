2f7ca5c7b8986b16faf124b737c5a3d7a0b41fb8
Yuriy Gerasimenko <Yuriy.Gerasimenko@tatar.ru>
2021-09-07 18:18:09 +0300

    Добавил библиотеку bimg для работы с изображениями

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module imgoptimizer
 
 go 1.17
+
+require github.com/h2non/bimg v1.1.5 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/h2non/bimg"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hi!")
+	buffer, err := bimg.Read("image.jpg")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	newImage, err := bimg.NewImage(buffer).Convert(bimg.WEBP)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	if bimg.NewImage(newImage).Type() == "webp" {
+		fmt.Fprintln(os.Stderr, "The image was converted into webp")
+	}
+
+	bimg.Write("new.webp", newImage)
 }
